c5a65d855b579a1261a51d38322ae20a28fca7b9
Leo79Jp <leojparodi@gmail.com>
2025-06-16 22:46:59 -0300

    Complete el crud

--- schemas/frutas.js
+++ schemas/frutas.js
@@ -3,9 +3,9 @@ const { z } = require('zod')
 const frutaSchema = z.object({
     
     id: z.number().int().min(1, 'ID must be at least 1').max(100, 'ID must be at most 100').default(1),
-    imagen: z.string().min(1, 'Imagen must be at least 1 character').max(10, 'Imagen must be at most 10 characters').default('🍌'),
-    nombre: z.string().min(1, 'Nombre must be at least 1 character').max(10, 'Nombre must be at most 10 characters').default('Bananas'),
-    precio: z.number().int().min(1, 'Precio must be at least 1').max(1000, 'Precio must be at most 100').default(220)
+    imagen: z.string().min(1, 'Imagen must be at least 1 character').max(10, 'Imagen must be at most 10 characters'),
+    nombre: z.string().min(1, 'Nombre must be at least 1 character').max(10, 'Nombre must be at most 10 characters'),
+    precio: z.number().int().min(1, 'Precio must be at least 1').max(10000, 'Precio must be at most 100').default(220)
 })
 const validarFruta = (fruta) => {
   return frutaSchema.safeParse(fruta)
--- server.js
+++ server.js
@@ -1,10 +1,7 @@
 const { connect, disconnect } = require("./db/connection");
 const { ObjectId } = require("mongodb");
 const express = require("express");
-const {
-  validarFruta,
-  validarFrutaParcialmente,
-} = require("./schemas/frutas.js");
+const { validarFruta, validarFrutaParcialmente } = require("./schemas/frutas.js");
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -71,14 +68,67 @@ app.get("/frutas/precio/:precio", async (req, res) => {
   const precio = parseInt(req.params.precio);
   try {
     const frutas = await req.db.find({ precio: { $gte: precio } }).toArray();
-    if (!frutas) {
+    if (!frutas || frutas.length === 0) {
       res.status(404).json({ error: "No hay frutas" });
+    }else{
+      res.json(frutas);
     }
-    res.json(frutas);
-  } catch (err) {
+  } catch (err) { 
     res.status(500).json({ error: "Error al conectar a la base de datos" });
   }
 });
+
+app.post("/frutas",  async(req, res) => {
+  const fruta = validarFruta(req.body)
+  if (!fruta.success) {
+    console.log('entro por aca');
+    res.status(400).json({ error: fruta.error.message });
+  }
+  try {
+    await req.db.insertOne(fruta.data);
+    res.status(201).json(fruta.data);
+  } catch (err) {
+    res.status(500).json({ error: "Error al crear la fruta" });
+  }
+});
+
+app.delete("/frutas/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const {deletedCount} = await req.db.deleteOne({ _id: new ObjectId(id) });
+    if (deletedCount === 0) {
+      res.status(404).json({ error: "No hay fruta" });
+    } else {  
+      res.status(204).json({ message: "Fruta borrada" });
+    }
+  } catch (err) {
+    res.status(500).json({ error: "Error al borrar la fruta" });
+  }
+});
+
+app.patch("/frutas/:id", async (req, res) => {
+  const fruta = validarFrutaParcialmente(req.body);
+  const frutaNueva =  fruta.data;
+  const { id } = req.params;
+
+  if (!fruta.success) {
+    res.status(400).json({ error: fruta.error.message });
+  }
+
+  try {
+    const { value } = await req.db.findOneAndUpdate({ _id: new ObjectId(id) }, 
+      { $set: frutaNueva });
+
+    if (!{value}) {
+      res.status(404).json({ message: "No se encontro la fruta para actualizar" });
+    } else {
+      const frutaModificada = await req.db.findOne({ _id: new ObjectId(id) });
+      res.status(200).json({frutaModificada});
+    }
+  } catch (err) {
+    res.status(500).json({ error: "Error al actualizar la fruta" });
+  }
+}); 
 
 app.listen(port, () => {
   console.log(`http://localhost:${port}`);
